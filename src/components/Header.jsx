05d59228c9047076fdce548e8f49b9f0b1fa3af4
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-01-27 16:33:45 -0300

    requisito 09 (adicionado input de search e logica para renderisação ao clicar no botão)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header() {
   const location = useLocation();
-  // const match = useRouteMatch();
-  /* const titleLocation = (location.pathname.split('/')[1]);
-  const titleUpperLocation = titleLocation[0].toUpperCase()
-   + titleLocation.substring(1).replace('-', ' ') */
   const [title, setTitle] = useState('Meals');
   const history = useHistory();
 
@@ -41,15 +37,15 @@ function Header() {
       <button type="button" onClick={ () => history.push('/profile') }>
         <img data-testid="profile-top-btn" src={ profileIcon } alt="profile pic" />
       </button>
-      {/* <h2>{location.match.params}</h2> */}
+
       <h1 data-testid="page-title">
         {title}
       </h1>
+
       {(location.pathname === '/meals' || location.pathname === '/drinks') && (
-        <button type="button">
-          <img data-testid="search-top-btn" src={ searchIcon } alt="search pic" />
-        </button>
+        <SearchBar />
       )}
+
     </div>
   );
 }
