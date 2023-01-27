b576efcd66ad2888cf66e1f4332c1d63be3f6bbe
gustavowillig <gugawillig@hotmail.com>
2023-01-27 17:45:25 -0300

    Requisito 10 feito - Implementação dos elementos iniciais do serachbar, radio buttons e button Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,7 +17,47 @@ function SearchBar() {
       </button>
 
       {searchBar.inputSearch && (
-        <input type="text" data-testid="search-input" />
+        <div>
+          <label htmlFor="search-text">
+            <input
+              type="text"
+              name="search-text"
+              data-testid="search-input"
+            />
+          </label>
+
+          <label htmlFor="ingredient">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="ingredient"
+              data-testid="ingredient-search-radio"
+            />
+            Ingredient
+          </label>
+
+          <label htmlFor="name">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="name"
+              data-testid="name-search-radio"
+            />
+            Name
+          </label>
+
+          <label htmlFor="first-letter">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="first-letter"
+              data-testid="first-letter-search-radio"
+            />
+            First letter
+          </label>
+
+          <button type="button" data-testid="exec-search-btn">Search</button>
+        </div>
       )}
 
     </>
