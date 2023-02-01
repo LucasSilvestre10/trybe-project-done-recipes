8df1c6b72e1ab1e3e748418fe8c8fb2c840055da
Priscila Rosa de Jesus <priscila.rosa.jesus@gmail.com>
2023-02-01 20:57:15 -0300

    css pagina de profile feita

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { GrFavorite } from 'react-icons/gr';
+import { BiLogOutCircle } from 'react-icons/bi';
+import { GiCook } from 'react-icons/gi';
+import { BsCheck2Circle } from 'react-icons/bs';
 import useLocalStorage from '../hooks/useLocalStorage';
-//
+import '../css/Profile.css';
 
 function Profile() {
   const [profile, setProfile] = useState({ email: 'Sem Email cadastrado' });
@@ -26,28 +30,43 @@ function Profile() {
   };
 
   return (
-    <div>
-      <p data-testid="profile-email">{profile.email}</p>
-      <button
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/done-recipes') }
+    <div className="container-profile">
+      <p
+        className="profile-email"
+        data-testid="profile-email"
       >
-        Done Recipes
-      </button>
-      <button
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
+        <GiCook className="cook" />
+        <i>{profile.email}</i>
 
-      </button>
-      <button
-        data-testid="profile-logout-btn"
-        onClick={ () => { logout(); } }
-      >
-        Logout
+      </p>
+      <div className="buttons-profile">
+        <button
+          className="profile-btn-recipes"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+          <BsCheck2Circle className="doneRcipes" />
+        </button>
+        <button
+          className="profile-btn-favorite-recipes"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+          <GrFavorite className="favorite" />
 
-      </button>
+        </button>
+        <button
+          className="profile-btn-logout"
+          data-testid="profile-logout-btn"
+          onClick={ () => { logout(); } }
+        >
+          Logout
+          <BiLogOutCircle className="logout" />
+
+        </button>
+      </div>
     </div>
   );
 }
