0c57ce6c5a2ad2e768e4b653a7650813adfcc248
Alysson Chiarelly <alyssonclr@gmail.com>
2023-02-04 20:13:16 -0300

    Implementação da tela de receitas favoritas, cards desenvolvidos

--- src/components/FavoriteRecipesCard.jsx
+++ src/components/FavoriteRecipesCard.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipesCard() {
+  const [recipes, setRecipes] = useState();
+  const { getLocalStorage } = useLocalStorage();
+
+  useEffect(() => {
+    const localRecipes = getLocalStorage('favoriteRecipes');
+    if (localRecipes.length !== 0) {
+      setRecipes(localRecipes);
+    }
+  }, []);
+
   return (
     <div>
       <button
@@ -19,34 +32,84 @@ function FavoriteRecipesCard() {
         Drinks
       </button>
       <div>
-        <img
-          src="/"
-          alt="Imagem de teste"
-          data-testid={ `${index}-horizontal-image` }
-        />
-        <span
-          data-testid={ `${index}-horizontal-top-text` }
-        >
-          Aqui é o texto da categoria da receita
-        </span>
-        <span
-          data-testid={ `${index}-horizontal-name` }
-        >
-          Aqui é o nome da receita
-
-        </span>
-        <button
-          data-testid={ `${index}-horizontal-share-btn` }
-        >
-          Compartilhar
-
-        </button>
-        <button
-          data-testid={ `${index}-horizontal-favorite-btn` }
-        >
-          Favoritar
-
-        </button>
+        {recipes ? (
+          recipes.map((recipe, index) => {
+            switch (recipe.type) {
+            case 'meal':
+              return (
+                <div key={ index }>
+                  <img
+                    src={ recipe.image }
+                    alt="Imagem"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                  <span
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {recipe.name}
+                  </span>
+                  <span
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    {`${recipe.nationality} - ${recipe.category}`}
+                  </span>
+                  <button>
+                    <img
+                      data-testid={ `${index}-horizontal-share-btn` }
+                      src={ shareIcon }
+                      alt="Share Icon"
+                    />
+                  </button>
+                  <button>
+                    <img
+                      data-testid={ `${index}-horizontal-favorite-btn` }
+                      src={ blackHeartIcon }
+                      alt="Black Heart Icon"
+                    />
+                  </button>
+                </div>
+              );
+            case 'drink':
+              return (
+                <div key={ index }>
+                  <img
+                    src={ recipe.image }
+                    alt="Imagem"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                  <span
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {recipe.name}
+                  </span>
+                  <span
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    { recipe.alcoholicOrNot }
+                  </span>
+                  <button>
+                    <img
+                      data-testid={ `${index}-horizontal-share-btn` }
+                      src={ shareIcon }
+                      alt="Share Icon"
+                    />
+                  </button>
+                  <button>
+                    <img
+                      data-testid={ `${index}-horizontal-favorite-btn` }
+                      src={ blackHeartIcon }
+                      alt="Black Heart Icon"
+                    />
+                  </button>
+                </div>
+              );
+            default:
+              return null;
+            }
+          })
+        ) : (
+          <p>Não há receitas favoritadas!</p>
+        )}
       </div>
     </div>
   );
